8ca60d47cc542cd4eacbc379fd877ada0a5d4bbf
eri <sergicrh@gmail.com>
2025-08-18 12:37:54 +0200

    feat: show only first 2 certificates on homepage

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -8,6 +8,7 @@ export type Certificate = {
 export const certificates: Certificate[] = [
   // Add your certificates here. Examples:
   // { title: "eJPT", issuer: "INE", date: "2023-06", href: "" },
+  { title: "DevSecOps Certificate", issuer: "TryHackMe", date: "2025-08", href: "https://tryhackme.com/certificate/THM-ZO7SIIOPPD" },
   { title: "Jr Penetration Tester", issuer: "TryHackMe", date: "2025-08", href: "https://tryhackme.com/certificate/THM-IY36HJQCSJ" },
   { title: "Cyber Security 101 Certificate", issuer: "TryHackMe", date: "2025-07", href: "https://tryhackme.com/certificate/THM-WVLWPABIQ7" },
   // { title: "AWS Certified Cloud Practitioner", issuer: "AWS", date: "2022-11", href: "" },
